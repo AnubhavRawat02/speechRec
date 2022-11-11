3168aacefa6ab75447e831fad5d124b8562fd7b8
Anubhav Rawat <anubhavrawat848@gmail.com>
2022-11-11 10:59:15 +0530

    Initial Commit

---
+++ speechRec/speechRecApp.swift
@@ -0,0 +1,17 @@
+//
+//  speechRecApp.swift
+//  speechRec
+//
+//  Created by Anubhav Rawat on 11/11/22.
+//
+
+import SwiftUI
+
+@main
+struct speechRecApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
